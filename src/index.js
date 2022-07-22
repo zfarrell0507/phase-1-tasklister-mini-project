e907538d5e3b4b0ca3381331c29136658d63bb8a
Zachary Farrell <zfarrell0507@gmail.com>
2022-07-22 14:33:36 -0400

    Initial tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildTask(e.target.children[1].value)
+    form.reset()
+  })
 });
+
+function buildTask(todo) {
+  let li = document.createElement('li')
+  li.textContent = `${todo} `
+  console.log(li)
+  document.querySelector('#tasks').appendChild(li)
+}
